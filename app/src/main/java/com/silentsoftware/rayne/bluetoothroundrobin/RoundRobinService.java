1f1ae46cf9f32a93eb388809547562523b675c05
AridRayne <aridrayne@gmail.com>
2015-04-21 13:21:47 -0700

    Updated Service Added IntentFilter to Service for Wifi Direct.

--- app/src/main/java/com/silentsoftware/rayne/bluetoothroundrobin/RoundRobinService.java
+++ app/src/main/java/com/silentsoftware/rayne/bluetoothroundrobin/RoundRobinService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.IBinder;
 
@@ -12,6 +13,7 @@ public class RoundRobinService extends Service {
     private WifiP2pManager mWifiManager;
     private WifiP2pManager.Channel mChannel;
     private BroadcastReceiver mBroadcastReceiver;
+    private IntentFilter mIntentFilter;
 
     public RoundRobinService() {
     }
@@ -21,6 +23,12 @@ public class RoundRobinService extends Service {
         mWifiManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         mChannel = mWifiManager.initialize(this, getMainLooper(), null);
         mBroadcastReceiver = new WifiDirectBroadcastReceiver(mWifiManager, mChannel, this);
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+        registerReceiver(mBroadcastReceiver, mIntentFilter);
         super.onCreate();
     }
 
@@ -32,6 +40,7 @@ public class RoundRobinService extends Service {
 
     @Override
     public void onDestroy() {
+        unregisterReceiver(mBroadcastReceiver);
         super.onDestroy();
     }
 
